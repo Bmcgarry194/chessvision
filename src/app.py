95dbb894a9384dacbc04f9cd763c1f2355d5bba2
Brian McGarry <bmcgarry816@gmail.com>
2018-08-22 16:05:11 -0700

    added game piece graph and updated layout

--- src/app.py
+++ src/app.py
@@ -5,52 +5,110 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 from datetime import datetime as dt
-import chess_stats as ps
+import chess_stats as cs
 import json
 
-app = dash.Dash('Chess Stats')
+app = dash.Dash('Chess Vision')
 
-eco_names = ps.eco_labels('../data/eco_names.json')
+eco_names = cs.eco_labels('../data/eco_names.json')
+
+#Layout of App
 
 app.layout = html.Div([
-    html.H1('Chess Analysis'),
+    html.H1('Chess Vision'),
+    
+    dcc.Markdown('''
+>
+> “You may learn much more from a game you lose than from a game you win. You will have to lose hundreds of games before becoming a good player.” – José Raúl Capablanca
+>
+'''),
+    
+    dcc.Markdown('''To begin, type in your Chess.com username and hit submit (If you do not have a Chess.com account but would like to see the graphs, feel free to check out my games with my username: cesdaycart). This may take several seconds if you have a lot of games. Next, use the dropdown menu to select which time control you would like to view. Once this is done the graphs will begin to populate. You can select individual games from the Elo graph to show the piece value difference over the course of that game. All graphs are interactive and can be panned, zoomed, and selected to get a better look at your data. Good luck!'''),
+                 
+    #User name Input box
     dcc.Input(
         id='input-box',
         placeholder='Enter player name...',
         type='text',
-        value='cesdaycart'
     ),
+    
+    #Submit button
     html.Button('Submit', id='button'),
-
-    dcc.Dropdown(id='my-dropdown'),
-
+    
+    #Dropdown menu to select a time control after inputing username
     html.Div([
-        html.H2('Games Played'),
-        dcc.Graph(id='game_count_graph')
-    ], style={'width': '45%', 'display': 'inline-block'}),
-
+        dcc.Dropdown(id='my-dropdown'),
+    ], style={'width': '20%', 'marginBottom': 50, 'marginTop': 25}),
+    
+    #Graph of difference in piece values over the course of a selected game
+    html.Div([
+        html.H2('Piece Value Difference'),
+        dcc.Graph(style={'height': '70vh'}, id='piece_difference_graph')
+    ], style={'width': '50%', 'display': 'inline-block', 'height': '90vh'}),
+    
+    #Graph of games played with time played on the y and rating on x axis
     html.Div([
         html.H2('Elo'),
-        dcc.Graph(id='elo_graph')
-    ], style={'display': 'inline-block', 'width': '45%'}),
+        dcc.Graph(style={'height': '70vh'}, id='elo_graph')
+    ], style={'display': 'inline-block', 'width': '50%','height': '90vh'}),
 
-    html.H1('Opening Frequency per Week'),
-
+    #Graph of the amount of times an opening was used by day
     html.Div([
-        dcc.Graph(id='opening_graph'),
-        ]),
+    html.H1('Opening Frequency'),
+    dcc.Graph(style={'width': '90vw', 'height': '80vh'}, id='opening_graph'),
+    ], style={'height': '90vh'}),
+    
+    #Graph of games played per week
+    html.Div([
+        html.H1('Games Played'),
+        dcc.Graph(style={'width': '90vw', 'height': '80vh'}, id='game_count_graph')
+    ], style={'height': '90vh'}),
+    
+    html.Div([
+        html.H1('Piece Count'),
+        dcc.Markdown('''
+        * This graph is more visually interesting than anything
+            * Each line represents one game you have played. Its ups and downs corresponding with the ebb and flow of material on the board
+            * Purple lines are games that you have lost while blue lines are games that you won
+        *The first dropdown lets you pick if you want to view games you played as white or black
+        *Pieces are given a numerical value:
+            *Pawns = 1
+            *Bishops and Knights = 3
+            *Rooks = 5
+            *Queen = 9
+        *The second dropdown lets you indicate how you would like to calculate the material value
+            *Difference takes the difference in material where a negitve number means that material favors black, while a positive number indicates white is up in material
+            *Total takes the total material value of all pieces on the board
+            *Black just gives the black material count
+            *White gives the white material count'''),
+        html.Div([
+        dcc.Dropdown(id='color_dropdown',
+                     placeholder='Select games you played as white or black...',
+                     options=[{'label': color, 'value': color} for color in ['black', 'white']],),
+        dcc.Dropdown(id='kind_dropdown',
+                     placeholder='Select type of piece value evaluation...',
+                    options=[{'label': label, 'value': value} for label, value in [('Difference in Piece Value', 0),
+                                                                                   ('Total piece value', 1),
+                                                                                   ('Black piece value', 2),
+                                                                                   ('White piece value', 3)]],)
+        
+        ], style={'width': '40%', 'marginBottom': 25, 'marginTop': 25}),
+        
+        dcc.Graph(style={'width': '90vw', 'height': '80vh'}, id='piece_count_graph')
+    ], style={'height': '90vh'}),
 
     #hidden div to store player data
     html.Div(id='player_data', style={'display': 'none'}),
 ])
 
-#gets player username from input box then makes api call to chess.com
-#for player data
+#Updating graphs with new data
+
+#gets player username from input box then makes api call to chess.com for player data
 @app.callback(Output('player_data', 'children'),
               [Input('button', 'n_clicks')],
               [State('input-box', 'value')])
 def change_player_data(n_clicks, value):
-    df = ps.game_stats_df(value)
+    df = cs.game_stats_df(value)
     return df.to_json()
 
 @app.callback(Output('my-dropdown', 'options'),
@@ -60,32 +118,38 @@ def update_dropdown(player_data):
     time_controls = dff['time_class'].unique()
     return [{'label': time_control, 'value': time_control} for time_control in time_controls]
 
+@app.callback(Output('piece_difference_graph', 'figure'),
+              [Input('elo_graph', 'clickData')])
+def update_piece_difference_graph(game_data):
+    x = list(range(len(game_data['points'][0]['customdata'])))
+    y = game_data['points'][0]['customdata']
+    return {'data': [go.Scatter(x=x,
+                                y=y,
+                                fill='tonexty',
+                                line={'shape': 'spline'})
+                    ]}
+
 @app.callback(Output('elo_graph', 'figure'),
               [Input('my-dropdown', 'value'),
                Input('player_data', 'children')])
-def update_ratings_graph(time_control_options, player_data):
+def update_elo_graph(time_control_options, player_data):
     dff = pd.read_json(player_data).sort_values(by=['end_time'])
     filtered_df = dff[(dff['rules'] == 'chess') & (dff['time_class'] == time_control_options)]
     return {'data': [go.Scatter(
                                 x=filtered_df['end_time'],
                                 y=filtered_df['player_rating'].values,
                                 text=filtered_df['player_color'],
+                                customdata=filtered_df['pieces_diff'],
                                 mode='lines+markers',
-                                marker={
-                                    'size': 15,
-                                    'color': filtered_df['rating_difference'],
-                                    'line': {'width': 0.5, 'color': 'white'},
-                                    'colorscale': 'Viridis',
-                                    'showscale': True,
-                                    'opacity': 0.5,})
+                                marker={'size': 5}),
                                     ],
-        'layout': {'margin': {'l': 40, 'r': 40, 't': 70, 'b': 30}}
+            'layout': go.Layout(hovermode = 'closest')
     }
 
 @app.callback(Output('game_count_graph', 'figure'),
               [Input('my-dropdown', 'value'),
                Input('player_data', 'children')])
-def update_games_graph(time_control_options, player_data):
+def update_game_count_graph(time_control_options, player_data):
     dff = pd.read_json(player_data)
     filtered_df = dff[(dff['rules'] == 'chess') & (dff['time_class'] == time_control_options)]
     number_of_games= filtered_df.resample('D', on='end_time').size().values
@@ -95,8 +159,7 @@ def update_games_graph(time_control_options, player_data):
             'x': dates,
             'y': number_of_games,
             'type': 'bar'
-        }],
-        'layout': {'margin': {'l': 40, 'r': 40, 't': 70, 'b': 30}}
+        }]
     }
 
 @app.callback(Output('opening_graph', 'figure'),
@@ -106,28 +169,72 @@ def update_openings_graph(time_control_options, player_data):
     dff = pd.read_json(player_data)
     dff['end_time'] = pd.to_datetime(dff['end_time'])
     filtered_df = dff[(dff['rules'] == 'chess') & (dff['time_class'] == time_control_options)]
+                          
     openings = filtered_df['eco'].unique()
 
     traces = []
     for opening in openings:
-        x = filtered_df.resample('W', on='end_time').mean().index
-        y = filtered_df[filtered_df['eco'] == opening].resample('W', on='end_time').size()
-        if y.sum() > 1:
-            traces.append(go.Scatter(
-                x=x,
-                y=y,
-                mode='lines+markers',
-                marker={'opacity': 0.5},
-                name=eco_names[opening]
-        ))
+        x = filtered_df[filtered_df['eco'] == opening].resample('D', on='end_time').mean().index
+        y = filtered_df[filtered_df['eco'] == opening].resample('D', on='end_time').size()
+        traces.append(go.Bar(x=x, y=y, name=opening))
 
     return {'data': traces,
             'layout': go.Layout(
                     xaxis={'title': 'Date'},
                     yaxis={'title': 'Number of Games'},
-                    hovermode='closest'
+                    barmode='stack',
+                    hovermode = 'closest'
                 )}
 
+@app.callback(Output('piece_count_graph', 'figure'),
+             [Input('my-dropdown', 'value'),
+              Input('player_data', 'children'),
+              Input('color_dropdown', 'value'),
+              Input('kind_dropdown', 'value')])
+def update_piece_count_graph(time_control_options, player_data, color, kind):
+    df = pd.read_json(player_data)
+    win_color_df = df[(df['player_color'] == color) & (df['win'] == True)]
+    lose_color_df = df[(df['player_color'] == color) & (df['lose'] == True)]
+    
+    traces = []
+    
+    win_games = win_color_df['pgn'].values
+    for game in win_games:
+        if not game:
+            continue
+        score = cs.piece_count(game)
+        x = list(range(len(score[kind])))
+        y = score[kind]
+        traces.append(go.Scatter(x=x,
+                                 y=y,
+                                 mode = 'lines',
+                                 opacity = .12,
+                                 showlegend = False,
+                                 hoverinfo='none',
+                                 line = {'color': 'steelblue',
+                                         'shape': 'spline'}))
+        
+    lose_games = lose_color_df['pgn'].values
+    for game in lose_games:
+        if not game:
+            continue
+        score = cs.piece_count(game)
+        x = list(range(len(score[kind])))
+        y = score[kind]
+        traces.append(go.Scatter(x=x,
+                                 y=y,
+                                 mode = 'lines',
+                                 showlegend = False,
+                                 hoverinfo='none',
+                                 opacity = .1,
+                                 line = {'color': 'purple',
+                                         'shape': 'spline'}))
+        
+    return {'data': traces,
+        'layout': go.Layout(
+                xaxis={'title': 'Number of Moves'},
+                yaxis={'title': 'Piece Value'},
+            )}
 app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
 if __name__ == '__main__':
--- src/chess_stats.py
+++ src/chess_stats.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import json
-import random
-import datetime
 import requests
 import numpy as np
-import re
+import chess
+import chess.uci
+import chess.pgn
+import matplotlib.pyplot as plt
+from io import StringIO
 
 def get_monthly_archives(username):
     '''Returns a list of months in which the user played a game'''
+    
     try:
         response = requests.get(f'https://api.chess.com/pub/player/{username}/games/archives')
-        months_played = json.loads(response.content.decode('utf-8'))['archives']
-        return [month[-7:].split('/') for month in months_played]
+        return json.loads(response.content.decode('utf-8'))['archives'] 
     except:
         return []
         
@@ -20,13 +22,13 @@ def get_player_games(username):
     
     months = get_monthly_archives(username)
     games = []
-    if months:
-        for month in months:
-            year = month[0]
-            month_ = month[1]
-            response = requests.get(f'https://api.chess.com/pub/player/{username}/games/{year}/{month_}')
-            for game in json.loads(response.content.decode('utf-8'))['games']:
-                games.append(game)
+    if not months:
+        return games
+    
+    for month in months:
+        response = requests.get(month)
+        for game in json.loads(response.content.decode('utf-8'))['games']:
+            games.append(game)
     return games
 
 def game_stats_df(username):
@@ -37,15 +39,20 @@ def game_stats_df(username):
     player_rating = []
     rating_difference = []
     player_result = []
+    opponent_result = []
     player_username = []
+    opponent_username = []
     end_time = []
     pgn = []
     eco = []
-    rated = []
     time_class = []
     rules = []
-    game_length = []
     player_color = []
+    pieces_diff = []
+    pieces_total = []
+    pieces_black = []
+    pieces_white = []
+    url = []
 
     for i, game in enumerate(game_list):
         
@@ -55,43 +62,58 @@ def game_stats_df(username):
             pgn_data = game['pgn'].split('\n')
             pgn.append(pgn_data[-1])
             eco.append(pgn_data[7].split('"')[-2])
-            game_length.append(int(re.split("(\d+)\.+", game['pgn'])[-2]))
+            counts = piece_count(pgn_data[-1])
+            pieces_diff.append(counts[0])
+            pieces_total.append(counts[1])
+            pieces_black.append(counts[2])
+            pieces_white.append(counts[3])
 
         else:
             pgn.append(None)
             eco.append(None)
-            game_length.append(None)
+            pieces_diff.append(None)
+            pieces_total.append(None)
+            pieces_black.append(None)
+            pieces_white.append(None)
             
         if game['black']['username'].lower() == username.lower():
             player_rating.append(game['black']['rating'])
             rating_difference.append(game['black']['rating'] - game['white']['rating'])
             player_result.append(game['black']['result'])
+            opponent_result.append(game['white']['result'])
             player_username.append(game['black']['username'])
             player_color.append('black')
+            opponent_username.append(game['white']['username'])
         else:
             player_rating.append(game['white']['rating'])
             rating_difference.append(game['white']['rating'] - game['black']['rating'])
             player_result.append(game['white']['result'])
+            opponent_result.append(game['black']['result'])
             player_username.append(game['white']['username'])
             player_color.append('white')
+            opponent_username.append(game['black']['username'])
             
         end_time.append(pd.to_datetime(game['end_time'], unit='s'))
-        rated.append(game['rated'])
         time_class.append(game['time_class'])
         rules.append(game['rules'])
+        url.append(game['url'])
 
     df = pd.DataFrame({'player_username': player_username,
                        'player_color': player_color,
                        'player_rating': player_rating,
                        'rating_difference': rating_difference,
                        'player_result': player_result,
+                       'opponent_result': opponent_result,
                        'end_time': end_time,
-                       'rated': rated,
                        'time_class': time_class,
                        'rules': rules,
                        'pgn': pgn,
                        'eco': eco,
-                       'game_length':game_length})
+                       'opponent_username': opponent_username,
+                       'pieces_diff': pieces_diff,
+                       'pieces_total': pieces_total,
+                       'pieces_black': pieces_black,
+                       'pieces_white': pieces_white,})
     
 #Create three columns in the data frame indicating a win, draw, or loss with a True or False
     df['win'] = df['player_result'] == 'win'
@@ -113,3 +135,106 @@ def eco_labels(filepath):
     with open(filepath) as infile:
         eco_names = json.load(infile)
     return eco_names
+
+def piece_count(pgn_string):
+    '''Returns piece value balance for every move of the game.
+    each piece has a value: pawns(p) = 1, bishops(b)/knights(n) = 3, rooks(r) = 5, queen(q) = 9,
+    For every move the difference in piece value is calculate by white piece value - black piece value.
+    So a positive number means you are ahead in material and and negitive number indicates you are down in material.
+    
+    Arg: 
+        pgn_string: Takes a string of a chess game in pgn notation
+    Returns:
+        A tuple of lists of numbers corresponding to piece value at each move: (diff, total, black, white)
+            diff = Difference in piece value, white adv is a positive int and black adv is a negitive int
+            total = Total piece value for both players
+            black = black piece value
+            white = white piece value'''
+    
+    if not pgn_string:
+        return (None, None, None, None)
+    
+    pgn = StringIO(pgn_string)
+    game = chess.pgn.read_game(pgn)
+    board = game.board()
+
+    diff = []
+    total = []
+    black = []
+    white = []
+    for move in game.main_line():
+        board.push(move)
+        board_state = board.fen().split()[0]
+        
+        black_score = (board_state.count('p')*1 +
+                       board_state.count('b')*3 +
+                       board_state.count('r')*5 +
+                       board_state.count('n')*3 +
+                       board_state.count('q')*9
+                      )
+        white_score = (board_state.count('P')*1 +
+                       board_state.count('R')*5 +
+                       board_state.count('B')*3 +
+                       board_state.count('N')*3 +
+                       board_state.count('Q')*9
+                      )
+        diff.append(white_score - black_score)
+        total.append(black_score + white_score)
+        black.append(black_score)
+        white.append(white_score)
+    
+    return (diff, total, black, white)
+
+def move_evaluation(pgn_string, engine_path, evaluation_time=5000, output='list'):
+    '''Takes pgn and returns engine evaluation
+    Params:
+        pgn_string: A string of the pgn
+        engine_path: A string of the path to your engine
+        evaluation_time: How long the engine will spend evaluating each move (in ms), default is 5000 ms (5 seconds)
+        output: either 'list' (list of evaluation values) or 'graph' (graph of evaluation values)
+    Returns: 
+        If output == list: A list of numbers corresponding to the engine evaluation at each move of the game, positive favors white and negitive favors black
+        If output == graph'''
+        
+    pgn = StringIO(pgn_string)
+    game = chess.pgn.read_game(pgn)
+
+    board = game.board()
+
+    centipawn_eval = []
+    #Loop over all game nodes:
+    while not game.is_end():
+        node = game.variations[0]
+        board = game.board() 
+        game = node
+
+        #load your engine:
+        handler = chess.uci.InfoHandler()
+        engine = chess.uci.popen_engine(engine_path) 
+        engine.info_handlers.append(handler)
+
+        #give position to the engine:
+        engine.position(board)
+
+        #Set your evaluation time, in ms:
+        evaluation = engine.go(movetime=evaluation_time)
+
+        if handler.info["score"][1][0]:
+            centipawn_eval.append(handler.info["score"][1][0])
+        else:
+            # mate in 1 = 6000 and is discounted(1/moves to mate) as the mate goes up as there as a high chance of not seeing the mate
+            mate_in = (1 / handler.info["score"][1][1]) * 6000 
+            centipawn_eval.append(mate_in)
+            
+    eval_list = [eval_ * -1 if idx % 2 != 0 else eval_ for idx, eval_ in enumerate(centipawn_eval)] 
+    
+    if output == 'list':
+        return eval_list
+    
+    elif output == 'graph':
+        fig, ax = plt.subplots()
+        x = list(range(len(eval_list)))
+        y = eval_list
+        ax.plot(x, y)
+        ax.fill_between(x, 0, y, alpha=0.2)
+        ax.axhline(0, ls='dashed', color='black')
